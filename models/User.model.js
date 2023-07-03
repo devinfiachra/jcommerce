1c736135ce9bc66094384511966bcc4a2df1e341
robopecha <julikolb@gmail.com>
2023-07-03 21:28:07 +0200

    footer and stuff

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,5 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
     username: {
@@ -26,4 +25,4 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
+module.exports = model("User", userSchema);
