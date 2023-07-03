552c9e84c3e0491be095baa33bb017471784f74c
robopecha <julikolb@gmail.com>
2023-07-03 12:41:25 +0200

    routes, controllers, views, models

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 require("dotenv").config();
+
 // ℹ️ Connects to the database
 require("./db");
+
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require("hbs");
+
 const app = express();
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+
 const capitalize = require("./utils/capitalize");
 const projectName = "jcommerce";
 
@@ -19,8 +25,7 @@ app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-
-// -30,6 +29,15 @@ app.use("/", indexRoutes);
+app.use("/", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
@@ -36,3 +41,5 @@ app.use('/', purchasesRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+module.exports = app;
