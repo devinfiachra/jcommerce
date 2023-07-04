9b7bf693cdd5093938c4d3ce3d0970085b50aa5f
Devin Fiachra <devinduffymeade@gmail.com>
2023-07-04 11:57:41 +0200

    update mongo configuration to fix timeout issue

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
 
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
-
-// Connect to the database before listening
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
-  });
+app.listen(PORT, () => {
+  console.log("listening for requests");
 });
