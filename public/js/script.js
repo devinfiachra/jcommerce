68fabbea5a34947e1f5545e4ef1320ac169ecd04
robopecha <julikolb@gmail.com>
2023-07-04 17:00:53 +0200

    detail product layout and buttons and unfinished things

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("jcommerce JS imported successfully!");
 });
+
+// star rating stars
+document.addEventListener("DOMContentLoaded", function() {
+  let ratingValue = product.rating;
+  let starsHTML = "";
+
+  for (let i = 1; i <= 5; i++) {
+      if (i <= ratingValue) {
+          starsHTML += '<i class="fas fa-star"></i>'; // Filled star
+      } else {
+          starsHTML += '<i class="far fa-star"></i>'; // Empty star
+      }
+  }
+
+  const ratingElement = document.querySelector(".rating");
+  ratingElement.innerHTML = starsHTML;
+});
